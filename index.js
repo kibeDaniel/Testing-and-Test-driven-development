0a84a2158603225354aace174eec4aa5bb56d4d8
KibeDaniel <danhepta@gmail.com>
2019-04-16 13:26:05 +0300

    replace content with letters in processedContetnt method

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function Phrase(content) {
 
   // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.content.toLowerCase();
+    return this.letters().toLowerCase();
   }
 
   // Returns the letters in the content.
